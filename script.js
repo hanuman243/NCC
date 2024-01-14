0cec4c77aee576429e4b02b7070af9313be5a4aa
hanuman243 <156604707+hanuman243@users.noreply.github.com>
2024-01-14 23:58:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add JavaScript functionality here
+document.getElementById('volunteer-btn').addEventListener('click', function() {
+    alert('Thank you for volunteering! We will be in touch with you.');
+});
