377262be2f4e2ef7e2659ed37edfa82786973609
hanuman243 <156604707+hanuman243@users.noreply.github.com>
2024-01-18 15:30:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// You can add JavaScript functionality here
-document.getElementById('volunteer-btn').addEventListener('click', function() {
-    alert('Thank you for volunteering! We will be in touch with you.');
-});
